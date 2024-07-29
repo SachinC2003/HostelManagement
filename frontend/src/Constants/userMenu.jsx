05c20bf46d17896a6f84d86b1db9350f9054e873
SachinC2003 <sachinchougule783@gmail.com>
2024-07-29 21:04:51 +0530

    aaded layout structure

---
+++ frontend/src/Constants/userMenu.jsx
@@ -0,0 +1,24 @@
+const userMenu =[
+    {
+        name : "Home",
+        path : "/courses",
+        icon:'ri-home-line'
+    },
+    {
+        name : "My-Courses",
+        path : "/mycourses",
+        icon:'ri-home-line'
+    },
+    {
+        name : "Applay Classes",
+        path : "/applay",
+        icon:'ri-home-line'
+    },
+    {
+        name : "Logout",
+        path : "/logout",
+        icon:'ri-logout-box-line'
+    }
+]
+
+export default userMenu;
--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -1,29 +1,58 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { useSetRecoilState } from 'recoil';
+import {userAtom} from '../Store/userAtom'
 import Input from '../Components/Input';
 import {Link} from "react-router-dom"
 import Dropdown from '../Components/Dropdown';
 
-const SignUp = () => {
+const Signup = () => {
+  const [email, setEmail] = useState('');
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [gender, setGender] = useState('');
+  const [role, setRole] = useState('')
 
-  const handleSubmit = (e) => {
+  const setUser = useSetRecoilState(userAtom);
+
+  const handleSignup = async(e) => {
     e.preventDefault();
-    console.log('Form submitted:', { firstName, lastName, username, password, gender });
-  };
+      try{
+        const response = await axios.post("http://localhost:3000/api/v1/user/signup",{
+           email, firstName, lastName, password, gender, role
+        })
+        console.log("signup req. response", response.data)
+
+        if(response.data)
+        {
+           localStorage.setItem('token', response.data.token);
+           setUser({
+             userId : response.data.userId,
+             role : response.data.role,
+             gender : response.data.gender
+           })
+           console.log("User state set:", { userId: response.data.userId, role: response.data.role, gender:response.data.gender });
+           navigate("/dashboard");
+         } else {
+           console.error("Authentication successful but token is missing in the response");
+           alert("Authentication successful, but there was an issue. Please try again.");
+         }
+       } catch (error) {
+         console.error('Authentication error:', error.response ? error.response.data : error);
+         alert(error.response?.data?.msg || "An error occurred. Please try again.");
+       }
+     };
 
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="w-full max-w-md p-8 bg-green-300 rounded shadow-md">
         <h2 className="mb-6 text-2xl font-bold text-center">Sign Up</h2>
-        <form onSubmit={handleSubmit}>
-          <Input placeholder="Ramesh" label="First Name" onChange={(e) => setFirstName(e.target.value)} />
-          <Input placeholder="Patil" label="Last Name" onChange={(e) => setLastName(e.target.value)} />
-          <Input placeholder="abc@gmail.com" label="Email" onChange={(e) => setUsername(e.target.value)} />
-          <Input placeholder="@@@@@" label="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
+        <form>
+          <Input placeholder="firstName" label="First Name" onChange={(e) => setFirstName(e.target.value)} />
+          <Input placeholder="lastName" label="Last Name" onChange={(e) => setLastName(e.target.value)} />
+          <Input placeholder="abc@gmail.com" label="Email" onChange={(e) => setEmail(e.target.value)} />
+          <Input placeholder="password" label="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
           
           <div className='flex justify-around'>
           
@@ -35,7 +64,7 @@ const SignUp = () => {
           <Dropdown 
             options={["User", "Owner"]}
             label="User Type"
-            onChange={(selected) => setUserType(selected)}
+            onChange={(selected) => setRole(selected)}
           />
             </div>
 
@@ -43,6 +72,7 @@ const SignUp = () => {
             <button
               type="submit"
               className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+              onClick={handleSignup}
             >
               Sign Up
             </button>
@@ -55,4 +85,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+export default Signup;
